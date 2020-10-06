e1bf358be25832c4d2cc93c89cd7f5c60328baa4
MultipleIllusionsi <seregaserega06@gmail.com>
2020-10-07 00:47:01 +0500

    initial commit

---
+++ components/AuthorIntro.js
@@ -0,0 +1,29 @@
+import { Row, Col, Media, Image } from "react-bootstrap";
+
+const AuthorIntro = () => (
+  <Row>
+    <Col md="8">
+      {/* AUTHOR INTRO STARTS */}
+      <Media className="mb-4 admin-intro">
+        <Image
+          roundedCircle
+          width={64}
+          height={64}
+          className="mr-3"
+          src="https://avatars2.githubusercontent.com/u/39622692?s=460&u=492c532ec1c742b387f719b865747afbabe50348&v=4"
+          alt="Generic placeholder"
+        />
+        <Media.Body>
+          <h5 className="font-weight-bold mb-0">Hello Friends,</h5>
+          <p className="welcome-text">
+            My name is Sergey Zakharov and I am an experienced software engineer
+            and this is my blog page.
+          </p>
+        </Media.Body>
+      </Media>
+      {/* AUTHOR INTRO ENDS */}
+    </Col>
+  </Row>
+);
+
+export default AuthorIntro;
